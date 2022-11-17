847d5e67f45b9d9b7e0ae6cf3ccc466d28b2a915
surya <110928161+surya3087@users.noreply.github.com>
2022-11-18 02:41:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let gif = document.getElementById('gif');
 let songItem = Array.from(document.getElementsByClassName('songItem')); //i can use songItem class or songCover class
 
 let song = [
-    {songName: 'rightnow', filePath: '/rightnow.mp3', coverPath: '/right now.jpg', duration: '4:45'},
+    {songName: 'rightnow', filePath: 'rightnow.mp3', coverPath: 'right now.jpg', duration: '4:45'},
     {songName: 'lonely', filePath: '/lonely.mp3', coverPath: '/lonely.jpg', duration: '4:45'},
     {songName: 'i wanna love you', filePath: 'audio/i wanna love you.mp3', coverPath: 'images/i wanna love you.jpg', duration: '4:45'},
     {songName: 'mama africa', filePath: 'audio/mama africa.mp3', coverPath: 'images/mama africa.jpg', duration: '4:45'},
